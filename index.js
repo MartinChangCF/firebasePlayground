e68b0736886c839a8846b4bad18ff6e5493f140e
Martin.ccf <martin@intrising.com.tw>
2018-10-05 15:26:25 +0800

    import lantech production

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 import admin from 'firebase-admin'
-import serviceAccount from './key/instaair-a188b-firebase-adminsdk-0rs2x-4f7f4cdee9.json'
+import serviceAccount from './static/key/lantechhub-firebase-adminsdk-bqmj9-48f8f5cb02'
+import production from './static/data/lantechProduction.json'
 
 // import axios from 'axios'
-// import _ from 'lodash'
+import _ from 'lodash'
 
-function initial () {
-  admin.initializeApp({
+const config = {
+  instaair: {
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://instaair-a188b.firebaseio.com'
-  })
+  },
+  intrihub: {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://intrihub-98da3.firebaseio.com'
+  },
+  lantechhub: {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://lantechhub.firebaseio.com'
+  }
+}
 
-  main()
+init()
+
+function init () {
+  admin.initializeApp(config.lantechhub)
+  const db = admin.database()
+  main(db)
+}
+
+async function main (db) {
+  console.time('Main process')
+
+  await organizeData(db)
+  await organizeFamily(db)
+
+  console.timeEnd('Main process')
+  close(db)
 }
 
 function close (db) {
@@ -18,28 +43,36 @@ function close (db) {
   process.exit()
 }
 
-function objPrint (obj) {
-  console.dir(obj, { depth: null })
-}
+// function objPrint (obj) {
+//   console.dir(obj, { depth: null })
+// }
 
-async function main () {
-  console.time('Main process')
-
-  const db = admin.database()
-
-  const obj = {
-    1: {
-      2: {
-        3: {
-          apple: 'pen'
-        }
-      }
+async function organizeData (db) {
+  const { attr, data } = production
+  let p = {}
+  for (let i in data) {
+    let id = data[i][0]
+    if (p[id] == null) p[id] = {}
+    for (let j in data[i]) {
+      let key = attr[j]
+      p[id][key] = data[i][j]
     }
+    p[id] = {
+      ...p[id],
+      createdAt: admin.database.ServerValue.TIMESTAMP,
+      updatedAt: admin.database.ServerValue.TIMESTAMP
+    }
+    console.log(p)
   }
-  objPrint(obj)
-
-  console.timeEnd('Main process')
-  close(db)
+  await db.ref('network/product').set(p)
 }
 
-initial()
+async function organizeFamily (db) {
+  const { data } = production
+  const f = {}
+  for (let i in data) {
+    f[data[i][3]] = true
+  }
+  console.log(f)
+  await db.ref('network/family').set(f)
+}
