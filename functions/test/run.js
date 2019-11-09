f28810ce6008a97e4c24d0c94e758dfd441e6a6e
Martin.ccf <martin@intrising.com.tw>
2019-11-09 18:32:58 +0800

    Add todo and tmp

--- functions/test/run.js
+++ functions/test/run.js
@@ -1,18 +1,36 @@
+// import axios from 'axios'
 import { schemas } from '../src/lib'
 
-let errors = schemas.reqBody.mib.validate({
-  version: '123',
-  custom: 'lantech_lantech',
-  models: [
-    {
+root()
+
+async function root () {
+  console.time('Start test')
+  console.group('Start test')
+
+  testMibSchema()
+
+  console.groupEnd()
+  console.timeEnd('Start test')
+}
+
+export function testMibSchema () {
+  console.time('MIB schema')
+  console.group('MIB schema')
+
+  let errors = schemas.reqBody.mib.validate({
+    version: '123',
+    custom: 'lantech_lantech',
+    models: [{
       category: 'swix1',
       layer2Url: '',
       layer3Url: '',
       model: 'APBP'
-    }
-  ],
-  status: 'release'
-})
+    }],
+    status: 'release'
+  })
 
-// console.log(errors.map(x => ({ [x.path]: x.message })))
-console.log(schemas.show(errors))
+  console.log(schemas.show(errors))
+
+  console.groupEnd()
+  console.timeEnd('MIB schema')
+}
--- index.js
+++ index.js
@@ -14,6 +14,18 @@ async function main () {
   console.time('Main process')
 
   await lantechHub.go20191107()
+  await lantechHub.go20191108()
+
+  /*
+    When handling firmware, there is a case needs to be aware of.
+    They have a scenario that is below:
+      A uses API create a firmware record with empty "feature", "bugfix"
+      B uses UI to update "feature", "bugfix"
+      A wants to update some of the props whcih cause the "feature", "bugfix" be erased.
+
+    You can create a partial update when using API, only effect some column so that the others will be neglect.
+    And UI can update the whole proprs...
+  */
 
   console.timeEnd('Main process')
   process.exit()
