bfcb69f11e38a05a9c5730e4077d3eaf33a6a891
Martin.ccf <martin@intrising.com.tw>
2019-11-10 16:56:53 +0800

    Pull daily task here - [index.js] - [lib/lantechHub.js] [lib/lantechHub.js] - complete reconstructMib - create test and reconstruct cluster function

--- index.js
+++ index.js
@@ -11,22 +11,33 @@ global.appRoot = resolve(__dirname)
 main()
 
 async function main () {
+  console.group('Main Process')
   console.time('Main process')
 
-  await lantechHub.go20191107()
-  await lantechHub.go20191108()
+  await go20191110()
 
   /*
-    When handling firmware, there is a case needs to be aware of.
-    They have a scenario that is below:
-      A uses API create a firmware record with empty "feature", "bugfix"
-      B uses UI to update "feature", "bugfix"
-      A wants to update some of the props whcih cause the "feature", "bugfix" be erased.
+  When handling firmware, there is a case needs to be aware of.
+  They have a scenario that is below:
+  A uses API create a firmware record with empty "feature", "bugfix"
+  B uses UI to update "feature", "bugfix"
+  A wants to update some of the props whcih cause the "feature", "bugfix" be erased.
 
-    You can create a partial update when using API, only effect some column so that the others will be neglect.
-    And UI can update the whole proprs...
+  You can create a partial update when using API, only effect some column so that the others will be neglect.
+  And UI can update the whole proprs...
   */
 
   console.timeEnd('Main process')
+  console.groupEnd()
   process.exit()
+}
+
+export async function go20191110 () {
+  console.time('2019/11/10')
+  console.group('2019/11/10')
+
+  await lantechHub.reconstructMib()
+
+  console.groupEnd()
+  console.timeEnd('2019/11/10')
 }
