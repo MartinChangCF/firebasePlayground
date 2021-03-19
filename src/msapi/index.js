804e0dfa03eda9e9ec93e83daedab847fbd41082
Martin.intrising <martin@intrising.com.tw>
2021-03-19 16:42:54 +0800

    test: msapi

---
+++ src/msapi/index.js
@@ -0,0 +1,96 @@
+import { throws } from 'assert'
+import axios from 'axios'
+import https from 'https'
+import key from './key.json'
+
+const ip = '192.168.1.1:1025'
+const apref = `https://${ip}/api/v1/`
+const testpool = [
+  'discovery/lldp/neighbour-information',
+  'docker/overview',
+  'eventlog/logs/log-data',
+  'maintenance/firmware/status',
+  'multicast/general/status',
+  'multicast/igmp/statistics',
+  'multicast/mld/groups',
+  'multicast/mld/statistics',
+  'ports/basic/status',
+  'redundant/stp/bridge-status',
+  'redundant/stp/port-status',
+  'redundant/loop-protection/status',
+  'redundant/g-8032-erps/status',,
+  'security/mac-address-tables/all-mac-addresses',
+  'security/ieee-802-1x/status',
+  'system/information',
+  'system/monitoring/data',
+  'system/monitoring/utilization',
+  'system/firmware/data',
+  'vlan/802-1q/status',
+  'vlan/mvrp/status'
+]
+
+const instance = axios.create({
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  })
+})
+
+async function login () {
+  const tk = await instance.post(apref+'login', key).then(r => {
+    const {status, statusText, data} = r
+    if (status === 200) {
+      return data.token
+    } else {
+      throw new Error(statusText)
+    }
+  }).catch(err => {
+    console.count("login failed")
+    console.error(err)
+  })
+  return tk
+}
+
+async function testall (token) {
+  const cfg = {
+    headers: {
+      Authorization: "Bearer " + token
+    }
+  }
+  const reqpool = testpool.map(url => {
+    return instance.get(apref+url, cfg)
+      .then(r => {
+        const {status} = r
+        if (status !== 200) {
+          throw new Error(statusText)
+        }
+      }).catch(err => {
+        console.count("request failed", url, err)
+      })
+  })
+  await Promise.all(reqpool)
+    .then(() => {
+      console.count('ok')
+    })
+    .catch(err => {
+      console.count('failed')
+      console.error(err)
+    })
+}
+
+async function main () {
+  console.group('Main Process')
+  console.time('Main process')
+
+  const count = Infinity
+  for (let i = 1; i <= count; i++) {
+    const tk = await login()
+    testall(tk)
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+
+  console.timeEnd('Main process')
+  console.groupEnd()
+  process.exit()
+}
+
+main()
---
+++ src/msapi/login.js
@@ -0,0 +1,68 @@
+import { throws } from 'assert'
+import axios from 'axios'
+import https from 'https'
+import formData from 'form-data'
+import key from './key.json'
+
+const ip = '192.168.1.1'
+const apref = `https://${ip}/`
+const auth = new formData()
+auth.append('account', key.account)
+auth.append('password', key.password)
+
+const instance = axios.create({
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  })
+})
+const cfg = {
+  headers: {
+    ...auth.getHeaders()
+  }
+}
+
+async function login () {
+  return instance.post(apref+'login', auth, cfg).then(r => {
+    const {status, statusText} = r
+    if (status === 200) {
+      console.count("login ok")
+    } else {
+      throw new Error(statusText)
+    }
+  }).catch(err => {
+    console.count("login failed")
+    console.error(err)
+  })
+}
+
+async function logout () {
+  return instance.post(apref+'logout').then(r => {
+    const {status, statusText} = r
+    if (status === 200) {
+      console.count("logout ok")
+    } else {
+      throw new Error(statusText)
+    }
+  }).catch(err => {
+    console.count("logout failed")
+    console.error(err)
+  })
+}
+
+async function main () {
+  console.group('Main Process')
+  console.time('Main process')
+
+  const count = Infinity
+  for (let i = 1; i <= count; i++) {
+    await login()
+    await logout()
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+
+  console.timeEnd('Main process')
+  console.groupEnd()
+  process.exit()
+}
+
+main()
